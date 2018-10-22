02c2f614d3c1aa6ceab6e2466efeff72fd38195e
corostegTest2 <42355839+corostegTest2@users.noreply.github.com>
2018-10-22 14:22:11 -0500

    Update cipher.spec.js

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -29,19 +29,4 @@ describe('cipher', () => {
     
   });
 
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      expect(typeof cipher.decode).toBe('function');
-    });
-
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',() =>{
-      expect(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG")).toBe("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-     });
-     it('debería retornar "ejemplo #1" para "lqltwsv #4" con offset 33',() =>{
-      expect(cipher.decode(33,"lqltwsv #4")).toBe("ejemplo #1"); 
-     }); 
-
-  });
-
 });
